d93d0595cbf2e5a49807f5221db694dae7ee6c0a
Matthew MAsters <mattdmasters@gmail.com>
2025-09-14 09:57:50 +0800

    refactoring

--- index.jsx
+++ index.jsx
@@ -1,129 +1,15 @@
 import { styled, React } from "uebersicht"
+import { initServer } from "./lib/server.js"
+import { updateState, initialState } from './lib/state.js'
+
+import { colors } from "./lib/util.js"
 import { Workspaces } from "./lib/components/workspaces.jsx"
 import { Battery } from "./lib/components/battery.jsx"
 import { Clock } from "./lib/components/clock.jsx"
-import { colors } from "./lib/util.js"
-import { StateEvents, initServer } from "./lib/server.js"
 import { Network } from "./lib/components/network.jsx"
 import { NowPlaying } from "./lib/components/nowplaying.jsx"
 
 const init = initServer
-
-const initialState = {
-	connected: false,
-	battery: {
-		percentage: 0,
-		source: "Unknown",
-		charging: false,
-	},
-	workspaces: [],
-	network: {
-		ssid: "",
-		connected: false,
-		showSSID: false
-	},
-	nowPlaying: {
-		isPlaying: false,
-		artist: "",
-		title: "",
-		player: ""
-	}
-}
-
-function updateState(event, state) {
-	switch (event.type) {
-		case StateEvents.Connecting: {
-			return {
-				...state,
-				connected: false
-			}
-		}
-		case StateEvents.Connected: {
-			return {
-				...state,
-				connected: true
-			}
-		}
-		case StateEvents.Battery: {
-			return {
-				...state,
-				battery: event.data,
-			}
-		}
-		case StateEvents.Workspaces: {
-			return {
-				...state,
-				workspaces: event.data
-			}
-		}
-		case StateEvents.SSIDChange: {
-			return {
-				...state,
-				network: {
-					ssid: event.data.ssid,
-					connected: true
-				}
-			}
-		}
-		case StateEvents.Connected: {
-			return {
-				...state,
-				network: {
-					...state.network,
-					connected: true
-				}
-			}
-		}
-		case StateEvents.WifiDisconneted: {
-			return {
-				...state,
-				network: {
-					ssid: "",
-					connected: false
-				}
-			}
-		}
-		case StateEvents.ShowSSID: {
-			return {
-				...state,
-				network: {
-					...state.network,
-					showSSID: !state.network.showSSID
-				}
-			}
-		}
-		case StateEvents.NowPlaying: {
-			return {
-				...state,
-				nowPlaying: {
-					...state.nowPlaying,
-					...event.data
-				}
-			}
-		}
-		case StateEvents.IsPlaying: {
-			return {
-				...state,
-				nowPlaying: {
-					...state.nowPlaying,
-					isPlaying: event.data
-				}
-			}
-		}
-		case StateEvents.CurrentPlayer: {
-			return {
-				...state,
-				nowPlaying: {
-					...state.nowPlaying,
-					player: event.data
-				}
-
-			}
-		}
-		default:
-			return state
-	}
-}
 
 const className = {
 	top: 0,
@@ -162,7 +48,13 @@ function Widget({ connected, battery, workspaces, network, nowPlaying }) {
 }
 
 function render(state) {
-	return <Widget connected={state.connected} battery={state.battery} workspaces={state.workspaces} network={state.network} nowPlaying={state.nowPlaying} />
+	return <Widget
+		connected={state.connected}
+		battery={state.battery}
+		workspaces={state.workspaces}
+		network={state.network}
+		nowPlaying={state.nowPlaying}
+	/>
 }
 
 export { className, initialState, init, updateState, render }
